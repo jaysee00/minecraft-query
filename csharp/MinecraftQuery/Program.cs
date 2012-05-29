dca311534a97ca26d184f09450e5fd69637a8e81
jclark <jclark@atlassian.com>
2012-05-30 09:42:04 +1000

    Made the serial port name configurable.

--- csharp/MinecraftQuery/Program.cs
+++ csharp/MinecraftQuery/Program.cs
@@ -8,29 +8,51 @@ namespace MinecraftQuery
 {
     class Program
     {
-        private const String portName = "/dev/tty.usbmodemfd121";
+        private const String DefaultPortName = "/dev/tty.usbmodemfd121";
         private const int baud = 9600;
 
         static void Main(string[] args)
         {
-            //var buff = new byte[] { 0x39, 0x35, 0x31, 0x33, 0x33, 0x30, 0x37 };
+            var portName = "";
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine("No port name specified. Looking for a suitable default.");
+                var ports = SerialPort.GetPortNames();
+                foreach (var port in ports)
+                {
+                    if (port.StartsWith("/dev/tty.usbmodem", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Console.WriteLine("Found port " + port);
+                        portName = port;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Skipping " + port);
+                    }
+                }
 
-            //int result = Convert.ToInt32(new String(Encoding.ASCII.GetChars(buff)));
-            //Console.WriteLine("signed result is " + result);
+                if (String.IsNullOrEmpty(portName))
+                {
+                    Console.WriteLine("Falling back to Default Port Name " + DefaultPortName);
+                    portName = DefaultPortName;
 
-            //UInt32 uresult = Convert.ToUInt32(new String(Encoding.ASCII.GetChars(buff)));
-            //Console.WriteLine("unsigned result is " + uresult);
-
-            //Console.ReadLine();
+                }
+            }
+            else
+            {
+                portName = args[0];
+                Console.WriteLine("Connecting to port " + portName);
+            }
+            
             var condition = new SettableCondition();
             condition.Value = false;
 
             var conduit = ConduitFactory//.Connect<MinecraftQueryMessage>(new RandomMessageGenerator())
-                                        .Connect<MinecraftQueryMessage>(new MinecraftQueryMessageGenerator("172.20.0.2", 25565))
+                                        .Connect<MinecraftQueryMessage>(new MinecraftQueryMessageGenerator("localhost", 25565))
                                         .With(new ArduinoMessageConsumer(portName, baud))
                                         //.With(new ConsoleMessageConsumer<MinecraftQueryMessage>())
                                         .StopOn(condition).Build();
-
             conduit.Channel();
 
             Console.WriteLine("Running.. any key to stop");
